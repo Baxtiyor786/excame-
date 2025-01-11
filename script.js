de803761cb9761f74e6939a8877dd039707095c0
Baxtiyor786 <jomoliddinovbaxtiyor@gmail.com>
2025-01-11 23:03:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let a = prompt ("1-raqamni kiriting")
+let b = prompt ("tealang \n 1)+ \n 2)/ \n 3)* \n  4)-")
+let c = prompt ("2-raqamni kiriting")
+
+let d1 = document.querySelector('.d1');
+let d2 = document.querySelector('.d2');
+let d3 = document.querySelector('.d3');
+
+d1.textContent = `${a}`
+d3.textContent = `${c}`
+
+let d22 = document.querySelector('.d22');
+let d33 = document.querySelector('.d33');
+
+if(b == "1"){
+    d2.textContent = '+'
+}else if(b == "2"){
+    d2.textContent = '/'
+}else if(b == "3"){
+    d2.textContent = '*'
+}else if(b == "4"){
+    d2.textContent = '-'
+}
+
+d22.addEventListener("click", function(){
+    if(b == "1"){
+        d2.textContent = '+'
+        d33.textContent = `Natija: ${a+c}`
+    }else if(b == "2"){
+        d2.textContent = '/'
+        d33.textContent = `Natija: ${a/c};`
+    }else if(b == "3"){
+        d2.textContent = '*'
+        d33.textContent = `Natija: ${a*c}`
+    }else if(b == "4"){
+        d2.textContent = '-'
+        d33.textContent = `Natija: ${a-c}`
+    }
+    d33.classList.toggle("d333");
+})
+
+
+
+// document.getElementById("myButton").addEventListener("click", function () {
+//     alert("Кнопка нажата!");
+// });
